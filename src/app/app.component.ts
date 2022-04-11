a87380b983a93a2ab41e556e3593ca63de9eb423
= <=>
2022-04-11 20:57:57 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Projeto';
+  public assunto = 'Angular';
+  public aula = 10;
+  public aux = 0;
+  mudarAssunto(){
+    if(this.aux%2==0){
+      this.assunto = 'PHP';
+      this.aux = 1;
+    }
+    else{
+      this.assunto = 'Angular';
+      this.aux = 1;
+    }
+
+  }
+
+  public div: boolean = true;
+  
 }
+
